f9d13f04e335f6017009e7a986fc7a8dd84f5e53
SeanJA <sean.sandy@gmail.com>
2012-11-17 13:24:07 -0400

    Read the file only once

--- index.php
+++ index.php
@@ -2,32 +2,39 @@
 
 require 'classes/markdown.php';
 
-$page = isset($_GET['page'])? $_GET['page']:'page-1';
+$page = isset($_GET['page']) ? $_GET['page'] : 'page-1';
 
 
 $m = new markdown();
 
-$file = 'pages/'.$page.'.md';
+$file = 'pages/' . $page . '.md';
 
-if(!file_exists($file)){
-	header('HTTP/1.0 404 Not Found');
-	$page = '404';
+if (!file_exists($file)) {
+    header('HTTP/1.0 404 Not Found');
+    $page = '404';
 }
-$source = file_get_contents($file);
 
 // read the first line
-$f = fopen($file,'r');
-$title = fgets($f);
+$f = fopen($file, 'r');
+$first = true;
+while (!feof ($f)) {
+	if($first){
+		$source = fgets($f);
+		$title = trim($source);
+		$first = false;
+	} else {
+		$source .= fgets($f);
+	}
+}
 fclose($f);
 
 // if the title is not Suzuki T500 Cobra MK I Rebuild, append it to the title
-if($title != 'Suzuki T500 Cobra MK I Rebuild'){
+if ($title != 'Suzuki T500 Cobra MK I Rebuild') {
     $title = $title . ' | Suzuki T500 Cobra MK I Rebuild';
 }
 // end of modifications
 
 $body = $m->transform($source);
-
 
 //the header source
 include 'templates/header.php';
