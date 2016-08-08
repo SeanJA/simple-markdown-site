ff15e711afae8af7ef928af1c4eb9ad857cd3e4c
SeanJA <sean.sandy@gmail.com>
2016-08-07 21:08:10 -0400

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -37,9 +37,9 @@
 </footer>
 </div>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
-        <script src="js/vendor/lightbox.js"></script>
-        <script src="js/plugins.js"></script>
+        <script src="<?php echo url('js/vendor/jquery-1.8.2.min.js') ?>"></script>
+        <script src="<?php echo url('js/vendor/lightbox.js') ?>"></script>
+        <script src="<?php echo url('js/plugins.js') ?>"></script>
         <script>
             var _gaq=[['_setAccount','UA-'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
